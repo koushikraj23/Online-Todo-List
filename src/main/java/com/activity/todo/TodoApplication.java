5f0947c11e0f0bd0e572b2b2ed5bc28fd21b1812
Koushikraj Rajendran <koushikraj2@gmail.com>
2020-08-12 21:54:53 +0100

    War changes made. Can be used as springboot standalone application or exported to war fro tomcat server

--- src/main/java/com/activity/todo/TodoApplication.java
+++ src/main/java/com/activity/todo/TodoApplication.java
@@ -2,13 +2,22 @@ package com.activity.todo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-public class TodoApplication {
+public class TodoApplication extends SpringBootServletInitializer{
 
 	public static void main(String[] args) {
 		SpringApplication.run(TodoApplication.class, args);
+	}
+	
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		// TODO Auto-generated method stub
+		return builder.sources(TodoApplication.class);
 	}
 
 }
